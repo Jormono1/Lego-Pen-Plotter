c6498de6fd272b69ccee2bab2b558fe72c59290a
Jormono1 <115799670+Jormono1@users.noreply.github.com>
2025-10-24 14:45:23 -0400

    Update coordinate for paper

--- PC_scripts/text_plotter/Plotter_Font.py
+++ PC_scripts/text_plotter/Plotter_Font.py
@@ -1,6 +1,6 @@
 machine_x_range = 2289        # Upper limit of machine travel in X direction (degrees rotation)
 machine_y_range = 3436        # Upper limit of machine travel in Y direction (degrees rotation)
-paper_x_min = 62    # defines starting corner of paper to be written on
+paper_x_min = 150    # defines starting corner of paper to be written on
 paper_y_min = 466
 paper_x_max = 2064  # defines opposite corner of paper from min, describes full range of paper
 paper_y_max = 1455
@@ -689,6 +689,7 @@ for line in lines:
     output.close()
     coordinates.clear()     # prevents re-writing old coordinates on new line file
     scrubbed_coordinates.clear()
+
 
 
 
