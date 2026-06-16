a883d57bd016b2626bd2890e543fc049c9ceb6ba
Jormono1 <115799670+Jormono1@users.noreply.github.com>
2026-06-16 19:28:44 -0400

    Update Plotter_Etch_A_Sketch_Mode.py
    * Add color indicator for B button function
    * moved homing sequence to menu/start button to prevent accidental button presses
    * added button combo to reset stick movement speed to initial value
    * changed hub light color scheme, red is b button, green is x button, blue is normal
    * changed value for where gantry moves when pressing B button

--- pybricks_hub_scripts/Plotter_Etch_A_Sketch_Mode.py
+++ pybricks_hub_scripts/Plotter_Etch_A_Sketch_Mode.py
@@ -1,14 +1,17 @@
 ### standard mode: Hub light = Blue, Left stick controls X and Y motors ###
-### alt mode: Hub light = Red, Left stick up/down controls Y motor, Right stick left/right controls X motor ###
-### Y button triggers homing sequence, X button Toggles standard/alt modes ###
+### alt mode: Hub light = Green, Left stick up/down controls Y motor, Right stick left/right controls X motor ###
+### Menu (start) button triggers homing sequence, X button Toggles standard/alt modes ###
 ### LB button reduces speed, RB button increases speed ###
 ### B button sends gantry to Y Max, intention is to clear way for changing paper, press B again to return to normal use ###
+### B button also changes the hub light to Red for visiual feedback ###
+### LB + RB combo resets stick speed to initial value ###
 
 ### IF FRESH BATTERIES change value on line 36 to 15, if not fresh revert to 18 ###
 from pybricks.hubs import TechnicHub
 from pybricks.iodevices import XboxController
 from pybricks.parameters import Direction, Port, Side, Stop, Button, Color
 from pybricks.pupdevices import Motor
+from pybricks.tools import wait
 
 X_Motor = Motor(Port.A, Direction.COUNTERCLOCKWISE)
 Y_Motor = Motor(Port.B)
@@ -29,7 +32,7 @@ allow_y_max = paper_y_max - tollerance
 pen_engaged = False
 stick_speed = 5                                                                 # user controlled speed, starts at 5 can range from 1 to 10 incremented by 0.5
 alt_control = False                                                             # allows user to toggle between two different control stick layouts
-button_wait = 200                                                               # prevents button bounce on inputs
+button_wait = 200                                                               # delay to prevent button bounce on inputs
 
 def homing(x_min, y_min):
     Z_Motor.run_until_stalled(move_speed,duty_limit=20)                         # raise pen
@@ -64,7 +67,7 @@ def y_can_move(stick):
         else:
             return 1
 
-def main(pen_engaged, stick_speed, x_min, y_min, alt_control, button_wait, move_speed):
+def main(pen_engaged, stick_speed, x_min, y_min, alt_control, button_wait, move_speed, hub):
     while True:
         if alt_control:                                                         # alt control is on, x on right stick y on left stick
             stick_y = xbox.joystick_left()[1]
@@ -72,7 +75,7 @@ def main(pen_engaged, stick_speed, x_min, y_min, alt_control, button_wait, move_
         else:                                                                   # standard control, x and y movement controlled by left stick
             stick_y = xbox.joystick_left()[1]
             stick_x = xbox.joystick_left()[0]
-        X_Motor.run(round(stick_speed * x_can_move(stick_x) * stick_x))         # round because stick speed can be a float
+        X_Motor.run(round(stick_speed * x_can_move(stick_x) * stick_x))         # round because stick speed value can be a float
         Y_Motor.run(round(stick_speed * y_can_move(stick_y) * stick_y))
         if Button.A in xbox.buttons.pressed():                                  # A button toggles pen engagment
             if pen_engaged:
@@ -83,23 +86,28 @@ def main(pen_engaged, stick_speed, x_min, y_min, alt_control, button_wait, move_
                 pen_engaged = True
         elif Button.B in xbox.buttons.pressed():                                # B button moves gantry back for changing the paper
             Z_Motor.run_until_stalled(move_speed,duty_limit=20)
-            Y_Motor.run_target(move_speed, paper_y_max + 250 , wait=True)
+            hub.light.on(Color.RED)
+            Y_Motor.run_target(move_speed, 2700 , wait=True)                    # 2700 was determined arbitrarily to be a good distance back to clear the card
             hold = True
             while hold:                                                         # B button a second time returns the gantry to the paper
                 if Button.B in xbox.buttons.pressed():
                     Y_Motor.run_target(move_speed, allow_y_max, wait=True)
+                    if alt_control:
+                        hub.light.on(Color.GREEN)
+                    else:
+                        hub.light.on(Color.BLUE)
                     hold = False
-        elif Button.Y in xbox.buttons.pressed():                                # Y button triggers homing sequence
-            homing(x_min, y_min)
         elif Button.X in xbox.buttons.pressed():                                # X button toggles control mode
             if alt_control:
                 hub.light.on(Color.BLUE)
                 alt_control = False
                 wait(button_wait)
             else:
-                hub.light.on(Color.RED)
+                hub.light.on(Color.GREEN)
                 alt_control = True
                 wait(button_wait)
+        elif Button.MENU in xbox.buttons.pressed():                             # menu (start) button triggers homing sequence
+            homing(x_min, y_min)
         elif Button.LB in xbox.buttons.pressed():                               # LB button decreases speed multiplier
             if stick_speed > 1:
                 stick_speed -= 0.5
@@ -107,6 +115,9 @@ def main(pen_engaged, stick_speed, x_min, y_min, alt_control, button_wait, move_
         elif Button.RB in xbox.buttons.pressed():                               # RB button increases speed multiplier
             if stick_speed < 10:
                 stick_speed += 0.5      
-                wait(button_wait)          
+                wait(button_wait)     
+        elif Button.LB and Button.RB in xbox.buttons.pressed():                 # reset stick speed to starting speed of 5
+            stick_speed = 5
+            wait(button_wait)     
 homing(allow_x_min, allow_y_min)
-main(pen_engaged, stick_speed, allow_x_min, allow_y_min, alt_control, button_wait, move_speed)
+main(pen_engaged, stick_speed, allow_x_min, allow_y_min, alt_control, button_wait, move_speed, hub)
